5262dcb2645d6b0de0d0df98ad551a6ba09028c4
Abdellah <abdoachafik@gmail.com>
2026-09-09 16:29:17 +0200

    Removing unused Packages

--- src/main/java/com/abdellah/demo/SearchController.java
+++ src/main/java/com/abdellah/demo/SearchController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
-
 @RestController 
 public class SearchController 
 {
